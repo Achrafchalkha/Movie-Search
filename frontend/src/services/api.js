2f5c76573e9ab968f0b42fecbb46f472630703d4
achraf <chalkhaachraf03@gmail.com>
2025-01-24 04:43:48 +0100

    Your commit message

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "90a7f488dac1bf82761bc5263e9fc60b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
